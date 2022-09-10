2c94a55a6b021c75af0b94473ce3914b2528cc01
Tiernan8r <tiernan8r@pm.me>
2022-09-10 18:47:58 +0100

    change to using open-elevation.com API

--- gpx_elev.py
+++ gpx_elev.py
@@ -1,48 +1,72 @@
+import json
+
 import gpxpy
 import numpy as np
 import requests
-import time
-import datetime
 
-API_KEY = 'ak_xIO8pzCi_ugMbNElpl1MXJVEq'  # Get a free key from www.gpxz.io.
-BATCH_SIZE = 50  # 512 for paid customers.
+BATCH_SIZE = 512  # 512 for paid customers.
 
 PAUSE_INTERVAL = 1
 
+URL = "https://api.open-elevation.com/api/v1/lookup"
+
 
 def gpxz_elevation(lats, lons):
-    '''Iterate over the coordinates in chunks, querying the GPXZ api to return
-    a list of elevations in the same order.'''
+    """Iterate over the coordinates in chunks, querying the GPXZ api to return
+    a list of elevations in the same order."""
+
+    print(f"Requesting elevation details for {len(lats)} coordinates...")
+
     elevations = []
     n_chunks = int(len(lats) // BATCH_SIZE) + 1
     lat_chunks = np.array_split(lats, n_chunks)
     lon_chunks = np.array_split(lons, n_chunks)
 
     num_chunks = len(lat_chunks)
-    print(f"Have {num_chunks} to request over...")
+    print(f"Have {num_chunks} chunks to request over...")
 
-    eta = datetime.timedelta(seconds=num_chunks*PAUSE_INTERVAL)
-    print(f"This will take ~{eta}")
-
+    i = 1
     for lat_chunk, lon_chunk in zip(lat_chunks, lon_chunks):
-        latlons = '|'.join(f'{lat},{lon}' for lat,
-                           lon in zip(lat_chunk, lon_chunk))
-        response = requests.post(
-            'https://api.gpxz.io/v1/elevation/points',
-            headers={'x-api-key': API_KEY},
-            data={'latlons': latlons},
-        )
-        response.raise_for_status()
-        elevations += [r['elevation'] for r in response.json()['results']]
+        print(f"Chunk {i}:")
+        i += 1
 
-        print(f"Waiting {PAUSE_INTERVAL} secs for next request...")
-        time.sleep(PAUSE_INTERVAL)
+        locations = []
+        for lat, lon in zip(lat_chunk, lon_chunk):
+            loc = {
+                "latitude": lat,
+                "longitude": lon,
+            }
+
+            locations.append(loc)
+
+        payload = json.dumps({
+            "locations": locations
+        })
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", URL, headers=headers, data=payload)
+
+        # DEBUGGING...
+        # print("LOCATIONS:", {"locations": json.dumps(locations)})
+        # print()
+        # print("REQUEST HEADER:", response.request.headers)
+        # print("REQUEST BODY:", response.request.body)
+        # print()
+        # print("RESPONSE HEADER:", response.headers)
+        # print("REPONSE BODY:", response.text)
+
+        response.raise_for_status()
+        elevations += [r["elevation"] for r in response.json()["results"]]
+
     return elevations
 
 
-def add_elevation_to_gpx(gpx_path, output_path='with_elevation.gpx'):
+def add_elevation_to_gpx(gpx_path, output_path="with_elevation.gpx"):
     # Load gpx.
-    # gpx_path = 'run.gpx'
+    # gpx_path = "run.gpx"
     with open(gpx_path) as f:
         gpx = gpxpy.parse(f)
 
@@ -57,5 +81,5 @@ def add_elevation_to_gpx(gpx_path, output_path='with_elevation.gpx'):
         point.elevation = elevation
 
     # Save gpx file.
-    with open(output_path, 'w') as f:
+    with open(output_path, "w") as f:
         f.write(gpx.to_xml())
--- main.py
+++ main.py
@@ -10,11 +10,10 @@ GPX_FILES = ["./gpxs/LEJOG.gpx",
 def main():
 
     for gpx in GPX_FILES:
-        print("WORKING ON:")
+        print("Working on:")
         print(gpx)
         gpx_elev.add_elevation_to_gpx(gpx, gpx)
-        print("Waiting 10s for next request...")
-        time.sleep(10)
+
 
 if __name__ == "__main__":
     main()
